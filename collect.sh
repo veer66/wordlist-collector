7cc513cb959bb8c812b82ffb1fa145bfb7bfea65
Vee Satayamas <vsatayamas@gmail.com>
2018-01-04 18:19:31 +0700

    first commit

---
+++ collect.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git clone https://github.com/kanaung/wordlists.git myanmar-wordlist
+git clone https://github.com/silnrsi/khmerlbdict.git
+git clone git@github.com:veer66/lao-dictionary.git
+git clone https://github.com/longman694/thai-words.git
